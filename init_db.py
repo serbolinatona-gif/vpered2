30f5a660c13a8b8c5e6aee5df9f6a78398d24167
serbolinatona-gif <serbolinatona@gmail.com>
2026-04-03 02:58:45 +0300

    Add files via upload

--- init_db.py
+++ init_db.py
@@ -1,45 +1,92 @@
 import sqlite3
 
+
 conn = sqlite3.connect("vpered.db")
+
 c = conn.cursor()
 
+
+# создаём таблицу пользователей
+
 c.execute("""
+
 CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-login TEXT UNIQUE,
-password TEXT,
-avatar TEXT
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    login TEXT UNIQUE,
+
+    password TEXT,
+
+    avatar TEXT
+
 )
+
 """)
 
+
+# создаём таблицу постов
+
 c.execute("""
+
 CREATE TABLE IF NOT EXISTS posts(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-title TEXT,
-content TEXT,
-image TEXT,
-created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-user_id INTEGER
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    title TEXT,
+
+    content TEXT,
+
+    image TEXT,
+
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    user_id INTEGER
+
 )
+
 """)
 
+
+# создаём таблицу комментариев
+
 c.execute("""
+
 CREATE TABLE IF NOT EXISTS comments(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-text TEXT,
-user_id INTEGER,
-post_id INTEGER
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    text TEXT,
+
+    user_id INTEGER,
+
+    post_id INTEGER
+
 )
+
 """)
 
+
+# создаём таблицу лайков
+
 c.execute("""
+
 CREATE TABLE IF NOT EXISTS likes(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-post_id INTEGER
+
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    user_id INTEGER,
+
+    post_id INTEGER
+
 )
+
 """)
+
 
 conn.commit()
+
 conn.close()
+
 print("DB ready")
+
